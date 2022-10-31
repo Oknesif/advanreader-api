eeea7b6060bfc9febb9d553b7d1bd9fe08a0e4c8
Dmitry Fisenko <oknesif@gmail.com>
2022-10-31 21:57:27 +0100

    Add gradle

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven("https://plugins.gradle.org/m2/")
+    }
+}
